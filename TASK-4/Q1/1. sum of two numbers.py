44838fbf7ce111fe7436639a4557cbd0a61a0d53
CHARISHMA-LOTTI <99647801+CHARISHMA-LOTTI@users.noreply.github.com>
2022-02-23 16:30:14 +0530

    Add files via upload

---
+++ TASK-4/Q1/1. sum of two numbers.py
@@ -0,0 +1,6 @@
+print("To find sum of two numbers")
+First_number = input("enter the 1st number : ")
+Second_number = input("enter the 2nd number : ")
+sum = (float(First_number) + float(Second_number))
+print("sum = ", sum)
+input()
