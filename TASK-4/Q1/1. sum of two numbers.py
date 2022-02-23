e16c20a979806b1892ce8f0f61280c9d662aef38
CHARISHMA-LOTTI <99647801+CHARISHMA-LOTTI@users.noreply.github.com>
2022-02-23 15:03:54 +0530

    Delete TASK-4 directory

--- TASK-4/Q1/1. sum of two numbers.py
+++
@@ -1,6 +0,0 @@
-print("To find sum of two numbers")
-First_number = input("enter the 1st number : ")
-Second_number = input("enter the 2nd number : ")
-sum = (float(First_number) + float(Second_number))
-print("sum = ", sum)
-input()
--- TASK-4/Q5/5.pattern.py
+++
@@ -1,5 +0,0 @@
-line_number = 1
-while line_number <= 5:
-    print(" " * (5 - line_number) + " *" * line_number)
-    line_number += 1
-input()
